3342fdba9d653f186d47cf13eab3bd325261beb5
Orest <galenzao@gmail.com>
2025-11-13 10:19:35 +0000

    Refactoring highlighting system

--- include/syntax_highlight.c
+++ include/syntax_highlight.c
@@ -19,16 +19,16 @@ typedef enum {
 
 const char *ansi_colors[] = {  
   [TOKEN_RESET]       = "\033[0m",
-  [TOKEN_KEYWORD]     = "\033[35m",      // Magenta
-  [TOKEN_TYPE]        = "\033[36m",      // Cyan  
+  [TOKEN_KEYWORD]     = "\033[35m",       // Magenta
+  [TOKEN_TYPE]        = "\033[36m",       // Cyan
   [TOKEN_PREPROCESSOR]= "\033[38;5;199m", // Pink
   [TOKEN_CONSTANT]    = "\033[38;5;173m", // Light orange
-  [TOKEN_STRING]      = "\033[32m",      // Green
+  [TOKEN_STRING]      = "\033[32m",       // Green
   [TOKEN_NUMBER]      = "\033[38;5;208m", // Orange
   [TOKEN_COMMENT]     = "\033[38;5;242m", // Gray
-  [TOKEN_OPERATOR]    = "\033[37m",      // White
-  [TOKEN_IDENTIFIER]  = NULL,            // No color (don't print anything)
-  [TOKEN_UNKNOWN]     = NULL,            // No color
+  [TOKEN_OPERATOR]    = "\033[37m",       // White
+  [TOKEN_IDENTIFIER]  = NULL,             // No color (don't print anything)
+  [TOKEN_UNKNOWN]     = NULL,             // No color
 };
 
 // Control flow and language keywords
@@ -66,12 +66,11 @@ static const char *c_constants[] = {
 };
 
 int classify_token(char *token, size_t size) {
-  return 0;
-
   // Check if the preprocessor
   if(token[0] == '#') {
     for(int i = 0; c_preprocessor[i] != NULL; i++) {
-      if(size == strlen(c_preprocessor[i]) && strcmp(token, c_preprocessor[i])) {
+      int prep_len = strlen(c_preprocessor[i]);
+      if(size >= prep_len && strncmp(token, c_preprocessor[i], prep_len) == 0) {
         return TOKEN_PREPROCESSOR;
       }
     }
@@ -79,21 +78,21 @@ int classify_token(char *token, size_t size) {
  
   // Checking for the keywords 
   for(int i = 0; c_keywords[i] != NULL; i++) {
-    if(size == strlen(c_keywords[i]) && strcmp(c_keywords[i], token) == 0) {
+    if(size == strlen(c_keywords[i]) && strncmp(token, c_keywords[i], size) == 0) {
       return TOKEN_KEYWORD;
     }
   }
 
   // Checking for the types
   for(int i = 0; c_types[i] != NULL; i++) {
-    if(size == strlen(c_types[i]) && strcmp(c_types[i], token) == 0) {
+    if(size == strlen(c_types[i]) && strncmp(token, c_types[i], size) == 0) {
       return TOKEN_TYPE;
     }
   }
 
-  // Checking for the types
+  // Checking for the constants
   for(int i = 0; c_constants[i] != NULL; i++) {
-    if(size == strlen(c_constants[i]) && strcmp(c_constants[i], token) == 0) {
+    if(size == strlen(c_constants[i]) && strncmp(token, c_constants[i], size) == 0) {
       return TOKEN_CONSTANT;
     }
   }
@@ -102,45 +101,83 @@ int classify_token(char *token, size_t size) {
 }
 
 void print_colored_token(char *token, size_t size, int type) {
-  const char *color = NULL; 
+  const char *color = ansi_colors[type];
 
-  switch (type) {
-    case TOKEN_KEYWORD:      color = ansi_colors[TOKEN_KEYWORD]; break;
-    case TOKEN_TYPE:         color = ansi_colors[TOKEN_TYPE]; break;
-    case TOKEN_STRING:       color = ansi_colors[TOKEN_STRING]; break;
-    case TOKEN_NUMBER:       color = ansi_colors[TOKEN_NUMBER]; break;
-    case TOKEN_COMMENT:      color = ansi_colors[TOKEN_COMMENT]; break;
-    case TOKEN_PREPROCESSOR: color = ansi_colors[TOKEN_PREPROCESSOR]; break;
-    case TOKEN_CONSTANT:     color = ansi_colors[TOKEN_CONSTANT]; break;
-    default: break;
-  }
-
-  write(STDOUT_FILENO, color, strlen(color));
+  if(color != NULL) write(STDOUT_FILENO, color, strlen(color));
   write(STDOUT_FILENO, token, size);
-  write(STDOUT_FILENO, ansi_colors[TOKEN_RESET], strlen(ansi_colors[TOKEN_RESET]));
+  if(color != NULL) write(STDOUT_FILENO, ansi_colors[TOKEN_RESET], strlen(ansi_colors[TOKEN_RESET]));
 }
 
 void syntax_highlight_and_print(char *line, int size) {
   int pos = 0;
-  int in_string = 0;
 
   while(pos < size) {
-    while(pos < size && line[pos] == ' ') {
-      write(STDOUT_FILENO, " ", 1);
+    // Handle whitespaces
+    while (pos < size && isspace((unsigned char)line[pos])) {
+      write(STDOUT_FILENO, &line[pos], 1);
       pos++;
     }
     
     int token_start = pos;
-    int type;
+    int type = TOKEN_UNKNOWN;
     size_t token_len = 0;
 
+    // Handle strings
     if(line[token_start] == '"') {
       type = TOKEN_STRING;
       pos++;
       while(pos < size && line[pos] != '"') pos++;
+      if (pos < size && line[pos] == '"') pos++;
       token_len = pos - token_start;
+      print_colored_token(line + token_start, token_len, type);
+      continue;
     }
+
+    // Handle comments
+    if(token_start + 1 < size && line[token_start] == '/' && line[token_start + 1] == '/') {
+      type = TOKEN_COMMENT;
+      token_len = size - pos;
       
-    print_colored_token(line + token_start, token_len, type);
+      print_colored_token(line + token_start, token_len, type);
+      break;
+    }
+    
+    // Handle numbers
+    if(isdigit(line[pos])) {
+      type = TOKEN_NUMBER;
+
+      while(pos < size && (isdigit(line[pos]) || line[pos] == '.' || tolower(line[pos]) == 'f' || tolower(line[pos]) == 'x')) {
+        pos++;  
+      }
+      
+      token_len = pos - token_start;
+      print_colored_token(line + token_start, token_len, type);
+      continue;
+    }
+
+    // Handle words: Keywords, types, operators, preprocessor
+    if(isalnum(line[pos]) || line[pos] == '_' || line[pos] == '#') {
+      // Handle preprocessor
+      if(line[pos] == '#') {
+        while(pos < size && (isalnum(line[pos]) || line[pos] == '_' || line[pos] == '#')) {
+          pos++;
+        }
+      }
+      else {
+        while(pos < size && (isalnum(line[pos]) || line[pos] == '_')) {
+          pos++;
+        }
+      }
+      
+      token_len = pos - token_start;
+      type = classify_token(line + token_start, token_len);
+
+      print_colored_token(line + token_start, token_len, type);
+      continue;
+    }
+
+    // Handle OPERATORS and PUNCTUATION
+    write(STDOUT_FILENO, &line[pos], 1);
+    pos++;
   }
 }
