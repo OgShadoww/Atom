32c52dff8255701369ab9cdca0bb2f2578f7d74a
Orest <galenzao@gmail.com>
2025-12-31 15:55:47 +0200

    Clean

--- include/syntax_highlight.c
+++ include/syntax_highlight.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
 
 typedef enum {
   TOKEN_RESET,
